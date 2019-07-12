14cb99fda3deff150f8aca8cec7bf5ab0bca44b3
GotohFumiaki <gf1010redgreen@gmail.com>
2019-07-12 12:30:36 +0900

    プレハブで設定したデータをプロパティから一括で取得できるように

--- Assets/Scripts/Managers/Battlemanagers/BattleManager.cs
+++ Assets/Scripts/Managers/Battlemanagers/BattleManager.cs
@@ -1,8 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using BattleArenaMock.Scripts.Monster;
 
 namespace BattleArenaMock.Assets.Scripts.Managers.Battlemanagers
 {
     public class BattleManager : MonoBehaviour
     {
+        [SerializeField] private MonsterStatus monsStatus = (default);
+
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start()
+        {
+            Debug.Log("Monster1 MP is " + monsStatus.MonsterStatusGroupProp.HP);
+        }
     }
 }
--- Assets/Scripts/Monster/MonsterStatus.cs
+++ Assets/Scripts/Monster/MonsterStatus.cs
@@ -3,18 +3,42 @@ using UnityEngine;
 
 namespace BattleArenaMock.Scripts.Monster
 {
-    struct MonsterStatusGroup
+    public struct MonsterStatusGroup
     {
         // HP
-        int hp;
+        public int HP{ get; private set; }
         // MP
-        int mp;
+        public int MP{ get; private set; }
         // 攻撃力
-        int strength;
+        public int Strength{ get; private set; }
         // 防御力
-        int defence;
+        public int Defence{ get; private set; }
+
+        public MonsterStatusGroup(int hp, int mp, int strength, int defence)
+        {
+            HP = hp;
+            MP = mp;
+            Strength = strength;
+            Defence = defence;
+        }
     }
     public class MonsterStatus : MonoBehaviour
     {
+        // モンスター関連
+        [SerializeField] private int hp;
+        [SerializeField] private int mp;
+        [SerializeField] private int strength;
+        [SerializeField] private int defence;
+        // モンスターに必要最低限のデータ構造体
+        private MonsterStatusGroup monsStatus;
+        public MonsterStatusGroup MonsterStatusGroupProp{ get; private set; }
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start()
+        {
+            MonsterStatusGroupProp = new MonsterStatusGroup(hp, mp, strength, defence);
+        }
     }
 }
