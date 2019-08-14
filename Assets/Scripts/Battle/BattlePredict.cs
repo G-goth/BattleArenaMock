10928fce1e304be77a417004551266af4d362d6a
GotohFumiaki <gf1010redgreen@gmail.com>
2019-08-14 15:23:20 +0900

    ステータスの総合値によってどれが勝っているかを出してるだけの機能の実装

--- Assets/Scripts/Battle/BattlePredict.cs
+++ Assets/Scripts/Battle/BattlePredict.cs
@@ -1,8 +1,7 @@
-using System.Linq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UniRx;
-using UniRx.Triggers;
 using BattleArenaMock.Scripts.Monster;
 using BattleArenaMock.Assets.Scripts.Managers.Battlemanagers;
 
@@ -23,13 +22,40 @@ namespace BattleArenaMock.Assets.Scripts.Battle
             // モンスターのGameObjectを取得
             battleManager = GetComponent<BattleManager>();
             var statusArray = battleManager.MonsterObjectListProp.Select(obj => obj.GetComponentInChildren<MonsterStatus>().GetMonsterStatusGroup().TotalScore).ToArray();
-            ReturnTotalScoreArray(statusArray, statusArray.Sum());
+            // ReturnTotalScoreArray(statusArray, statusArray.Sum());
+            for(int i = 0; i < 20; ++i)
+            {
+                TestRangeMethod(statusArray, statusArray.Sum());
+            }
         }
 
         private float[] ReturnTotalScoreArray(int[] totalScoreArray, int sum)
         {
-            // Debug.Log((float)totalScoreArray[0] / (float)sum);
+            var test = Mathf.RoundToInt(totalScoreArray.Select(totalScore => (float)totalScore / (float)sum * 100).Sum());
             return totalScoreArray.Select(totalScore => (float)totalScore / (float)sum).ToArray();
+        }
+        private void TestRangeMethod(int[] totalScoreArray, int sum)
+        {
+            int min = 0, max = 0;
+            Dictionary<string, (int min, int max)> testMap = new Dictionary<string, (int min, int max)>();
+            
+            testMap.Add("Monster1", (0, Mathf.RoundToInt((float)totalScoreArray[0] / (float)sum * 100)));
+            for(int i = 1; i < totalScoreArray.Length; ++i)
+            {
+                min = Mathf.RoundToInt((float)totalScoreArray[i - 1] / (float)sum * 100) + min + 1;
+                max = Mathf.RoundToInt((float)totalScoreArray[i] / (float)sum * 100) + min;
+                testMap.Add("Monster" + (i + 1), (min, max));
+            }
+
+            var randNum = Mathf.RoundToInt(UnityEngine.Random.Range(0.0f, max));
+            foreach(KeyValuePair<string, (int min, int max)> item in testMap)
+            {
+                if(item.Value.min < randNum & item.Value.max > randNum)
+                {
+                    Debug.Log(item.Key);
+                    break;
+                }
+            }
         }
     }
 }
